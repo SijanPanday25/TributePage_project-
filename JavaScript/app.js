74411d45e76e6ca9388b68cf4afb810c49af37f2
sijan panday <sijanpanday@live.co.uk>
2022-02-25 19:58:01 +0000

    mostly fnished, few bugs

---
+++ JavaScript/app.js
@@ -0,0 +1,33 @@
+
+/* =============== Menu Toggle =============== */
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* =============== Menu Open =============== */
+/* Check if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* =============== Menu Closed =============== */
+/* Check if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* =============== Remove Menu =============== */
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // Each time nav_lin is clicked it removes the show-menue class (CSS)
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
